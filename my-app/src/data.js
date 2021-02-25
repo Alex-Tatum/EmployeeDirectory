f80f94a6735968cbc2605a5849f30f503c31874e
Alex Tatum <tatum_alex@ymail.com>
2021-02-25 17:40:14 -0500

    updated files

---
+++ my-app/src/data.js
@@ -0,0 +1,55 @@
+import ronPic from "./Components/Images/Ron.jpg"
+import harryPic from "./Components/Images/Harry.jpg"
+import hermionePic from "./Components/Images/Hermione.jpg"
+import siriusPic from "./Components/Images/Sirius.jpg"
+import lunaPic from "./Components/Images/Luna.jpg"
+
+export default [
+    
+    {
+        "id": "1",
+        "name": "Ron Weasley",
+        "image": ronPic,
+        "phone": "248-111-1111",
+        "email": "ronweasley@gmail.com",
+        "DOB": "03/01/1980",
+        "gender": "male"
+    },
+    {
+        "id": "2",
+        "name": "Harry Potter",
+        "image": harryPic,
+        "phone": "248-222-2222",
+        "email": "harrypotter@gmail.com",
+        "DOB": "07/31/1980",
+        "gender": "male"
+    },
+    {
+        "id": "3",
+        "name": "Hermione Granger",
+        "image": hermionePic,
+        "phone": "248-333-3333",
+        "email": "hermionegranger@gmail.com",
+        "DOB": "09/19/1979",
+        "gender": "female"
+    },
+    {
+        "id": "4",
+        "name": "Sirius Black",
+        "image": siriusPic,
+        "phone": "248-444-4444",
+        "email": "siriusblack@gmail.com",
+        "DOB": "11/03/1959",
+        "gender": "male"
+    },
+    {
+    "id": "5",
+    "name": "Luna Lovegood",
+    "image": lunaPic,
+    "phone": "248-555-5555",
+    "email": "lunalovegood@gmail.com",
+    "DOB": "02/13/1981",
+    "gender": "female"
+    }
+
+]
